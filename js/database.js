8f0d78be935de4a3b2626a02cb298f0a6122cf9b
SSNBKZ <ksitishareefa14@gmail.com>
2022-11-06 18:51:11 +0800

    idk what i did le

--- js/database.js
+++ js/database.js
@@ -30,10 +30,10 @@ if (document.title == "Quiz") {
   progressButton.addEventListener("click", writeInput);
   inputField.addEventListener("keyup", function (e) {
     if (e.keyCode == 13) writeInput();
-  })
+  });
   function writeInput() {
     push(
-      ref(db, "quiz/" + document.location.href.split('?')[1].split('=')[1]),
+      ref(db, "quiz/" + document.location.href.split("?")[1].split("=")[1]),
       document.getElementById("inputField").value.toLowerCase()
     );
   }
@@ -62,15 +62,13 @@ if (document.title == "Bulletin Homepage") {
   //       // labelsAboveBackground: 'rgb(253,253,0)'
   //     },
   //   }).draw();
-  }
-
   getAllInputs();
   document.getElementById("products").addEventListener("change", getAllInputs);
   function getAllInputs() {
     var chosen = document.getElementById("products").value;
     onValue(ref(db, "quiz/" + chosen), (snapshot) => {
       var data = snapshot.val();
-      var top3 = []
+      var top3 = [];
       if (data != null) {
         axios
           .get("https://quickchart.io/wordcloud", {
@@ -83,20 +81,19 @@ if (document.title == "Bulletin Homepage") {
             // console.log(Object.values(data).join(" "))
             // console.log(response.data)
             document.getElementById("results").innerHTML = response.data;
-            var cnt = 0
+            var cnt = 0;
             for (let elem of document.getElementById("results").children[0]
               .children) {
               console.log(elem.getAttribute("font-size"));
-              if (cnt<3){
-                top3.push(elem.textContent)
-                cnt +=1
+              if (cnt < 3) {
+                top3.push(elem.textContent);
+                cnt += 1;
               }
             }
-            document.getElementById('podium0').innerText = "🥇"+top3[0] ;
-            document.getElementById('podium1').innerText = "🥈"+top3[1] ;
-            document.getElementById('podium2').innerText = "🥉"+top3[2] ;
+            document.getElementById("podium0").innerText = "🥇" + top3[0];
+            document.getElementById("podium1").innerText = "🥈" + top3[1];
+            document.getElementById("podium2").innerText = "🥉" + top3[2];
             // bar(["🥈"+top3[1], "🥇"+top3[0], "🥉"+top3[2]])
-
           })
           .catch((error) => {
             console.log(error.message);
@@ -104,20 +101,20 @@ if (document.title == "Bulletin Homepage") {
       }
     });
   }
-// }
-
-if (document.title == "Register") {
-  document.getElementById("regis").addEventListener("click", register);
-  function register() {
-    set(ref(db, "users/" + document.getElementById("username").value), {
-      username: document.getElementById("username").value,
-      fetus_nickname: document.getElementById("fetus").value,
-      duedate: document.getElementById("due").value,
-      email: document.getElementById("email").value,
-      password: document.getElementById("pw").value,
-    });
-  }
 }
+
+// if (document.title == "Register") {
+//   document.getElementById("regis").addEventListener("click", register);
+//   function register() {
+//     set(ref(db, "users/" + document.getElementById("username").value), {
+//       username: document.getElementById("username").value,
+//       fetus_nickname: document.getElementById("fetus").value,
+//       duedate: document.getElementById("due").value,
+//       email: document.getElementById("email").value,
+//       password: document.getElementById("pw").value,
+//     });
+//   }
+// }
 
 if (document.title == "Login") {
   document.getElementById("login").addEventListener("click", validate);
@@ -133,4 +130,24 @@ if (document.title == "Login") {
       }
     });
   }
+  document.getElementById("regis").addEventListener("click", register);
+  function register() {
+    console.log('regis')
+    set(ref(db, "users/" + document.getElementById("susername").value), {
+      username: document.getElementById("susername").value,
+      fetus_nickname: document.getElementById("fetus").value,
+      duedate: document.getElementById("due").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("pw").value,
+    });
+  }
 }
+
+// if (document.title == "Calender"){
+//   function writeBooking() {
+//     push(
+//       ref(db, "calender/" + ), //user
+//       document.getElementById("inputField").value.toLowerCase() // booking
+//     );
+//   }
+// }
